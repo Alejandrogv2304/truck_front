db697ca428df785d8a76889957d34781b7d3c138
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2025-09-16 13:44:42 -0500

    Base del proyecto

---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function Router() {
+  
+
+  return (
+    <>
+    <BrowserRouter>
+    
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default Router
