b99f48f05d48896387306ce9873efedce6ba0f6a
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2025-11-26 13:16:25 -0500

    Ajustes de estilo camion card

--- src/components/CamionCard.tsx
+++ src/components/CamionCard.tsx
@@ -1,11 +1,15 @@
 import type { CamionCardProps } from "../types";
 import { FaToggleOn } from "react-icons/fa6";
 import { MdEditSquare } from "react-icons/md";
+import { FaTruckMoving } from "react-icons/fa";
 
 
 export default function CamionCard({placa, modelo, estado, id_camion, onDelete, onEdit}: CamionCardProps) {
   return (
     <div className="bg-[#EDFAF2] font-ubuntu rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-200">
+
+        <FaTruckMoving  className="text-3xl text-green-900 mb-2"/>
+
         <div className="flex items-center justify-between mb-4">
             <h1 className="text-2xl font-bold text-green-800">{placa}</h1>
             <span className={`text-sm ${estado === "activo" ? "bg-green-700" : "bg-red-500"} text-white p-1 rounded-lg font-medium`}>
@@ -24,7 +28,7 @@ export default function CamionCard({placa, modelo, estado, id_camion, onDelete, 
               className="mr-6 focus:outline-none"
               aria-label="Eliminar camión"
             >
-              <FaToggleOn className="text-2xl cursor-pointer text-gray-600 hover:text-red-500 hover:scale-125 transition-all duration-200" />
+              <FaToggleOn className="text-4xl cursor-pointer text-gray-600 hover:text-red-500 hover:scale-125 transition-all duration-200" />
             </button>
             
             <button 
@@ -32,7 +36,7 @@ export default function CamionCard({placa, modelo, estado, id_camion, onDelete, 
               className="focus:outline-none"
               aria-label="Editar camión"
             >
-              <MdEditSquare className="text-2xl cursor-pointer text-gray-600 hover:text-green-600 hover:scale-125 transition-all duration-200"/>
+              <MdEditSquare className="text-4xl cursor-pointer text-gray-600 hover:text-green-600 hover:scale-125 transition-all duration-200"/>
             </button>
 
         </div>
